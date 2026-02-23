b5a798a62b9842ff7a1702d7b8959d14f45bb360
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2026-02-23 23:01:13 +0100

    [MERGE] added topgg

--- include/palette/services/topgg.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <dpp/dpp.h>
-
-namespace palette::services {
-
-void topgg_post_stats(dpp::cluster &bot);
-void task(dpp::cluster &bot);
-
-} // namespace palette::services
--- src/events/ready.cpp
+++ src/events/ready.cpp
@@ -1,22 +1,15 @@
 #include "palette/events/ready.hpp"
 #include "palette/commands/registry.hpp"
 #include "palette/services/env_utils.hpp"
-// #include "palette/services/topgg.hpp"
-// #include <functional> // std::ref
-// #include <thread>
 
 namespace palette::events {
-void wire_ready(dpp::cluster &bot) {
-    bot.on_ready([&bot](const dpp::ready_t &event) {
+void wire_ready(dpp::cluster &bot, topgg::client &topgg) {
+    bot.on_ready([&bot, &topgg](const dpp::ready_t &_) {
         const bool production = palette::services::is_production_environment();
 
-        // TODO returns 400 Bad Requests
-        // if (!production) {
-        //     std::cout << "[CRON] starting..." << std::endl;
-        //     static std::thread cron_thread(palette::services::task,
-        //                                    std::ref(bot));
-        //     cron_thread.detach();
-        // }
+        if (!production) {
+            topgg.start_autoposter();
+        }
 
         if (dpp::run_once<struct register_commands_once>()) {
             commands::register_commands(bot); // overwrite commands
