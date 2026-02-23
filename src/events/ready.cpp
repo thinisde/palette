4ad3d786f65373236992e4b5db423071a440e47b
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2026-02-23 23:03:44 +0100

    [MERGE] fixed if statement

--- src/events/ready.cpp
+++ src/events/ready.cpp
@@ -7,7 +7,7 @@ void wire_ready(dpp::cluster &bot, topgg::client &topgg) {
     bot.on_ready([&bot, &topgg](const dpp::ready_t &_) {
         const bool production = palette::services::is_production_environment();
 
-        if (!production) {
+        if (production) {
             topgg.start_autoposter();
         }
 
